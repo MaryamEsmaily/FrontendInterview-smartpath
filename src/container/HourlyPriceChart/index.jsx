857e6514c7848dba8a093ac490292615bb7f02f8
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2022-11-17 15:15:05 +0330

    add bar to HourlyPriceChart

--- src/container/HourlyPriceChart/index.jsx
+++ src/container/HourlyPriceChart/index.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { ResponsiveBar } from "@nivo/bar";
+import { Box } from "@mui/system";
 
 function HourlyPriceChart() {
-  return <div>HourlyPriceChart</div>;
+  const data = useMemo(() => [], []);
+  return (
+    <Box height={500} my={2} mx={1} p={3} borderRadius={4} bgcolor="#fff">
+      <ResponsiveBar
+        colors={["#15b89b", "#ffd967", "#f24c4b"]}
+        data={data ?? []}
+        keys={[]}
+        indexBy="time"
+        margin={{ top: 50, right: 50, bottom: 50, left: 60 }}
+        padding={0.5}
+        groupMode="grouped"
+        valueScale={{ type: "linear" }}
+        role="application"
+        ariaLabel="Nivo bar chart demo"
+        enableLabel={false}
+        borderRadius={4}
+      />
+    </Box>
+  );
 }
 
 export default HourlyPriceChart;
