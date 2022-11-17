899380f55644a387e8ae775e2bc5a952b3c5a40e
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2022-11-17 16:34:47 +0330

    update bar chart

--- src/component/BarChart/index.jsx
+++ src/component/BarChart/index.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { ResponsiveBar } from "@nivo/bar";
 
-function BarChart({ data, keys, colors }) {
+function BarChart({ data, keys, colors, padding = 0.5 }) {
   return (
     <ResponsiveBar
       colors={colors}
       data={data}
       keys={keys}
       indexBy="time"
-      margin={{ top: 50, right: 50, bottom: 50, left: 50 }}
-      padding={0.5}
+      margin={{ top: 10, right: 20, bottom: 30, left: 40 }}
+      padding={padding}
       groupMode="grouped"
-      valueScale={{ type: "linear" }}
-      role="application"
-      ariaLabel="Nivo bar chart demo"
       enableLabel={false}
       borderRadius={4}
     />
